5110bca27db490bbf19b4d5d510e7fbad3ef897b
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-11 18:47:34 -0500

    commit tree 9e6a8bdf04c7a642a720cb7b67304643bc62b506

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,10 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+  attr_accessor :name, :breed, :age
+
+  def initialize(key_value)
+    key_value.each do |key,value|
+      self.send("#{key}=",value)
+    end
+  end
+end
